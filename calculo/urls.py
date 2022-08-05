62949060cad1c8b833225340fb9da1a5e44f726a
Emerson Freire <emersonnascimento.freire@gmail.com>
2022-08-04 21:16:10 -0300

    Alterações no frontend

--- calculo/urls.py
+++ calculo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . views import imc_view
 
 urlpatterns = [
-    path('imc/', imc_view), #Esta URL, encaminha a requisição do usuário para a "view"
+    path('', imc_view), #Esta URL, encaminha a requisição do usuário para a "view"
 ]
